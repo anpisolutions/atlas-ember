09263da9ccc06eebc7d18496327a316b6646904a
erk-srs <erk-srs@outlook.com>
2016-08-30 22:01:48 +0530

    Cleanup code in displaying the task table and gates

--- app/pods/components/workflow-gates/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  isDisabled: false,
-  label: null,
-  value: false,
-
- 
-});
--- app/pods/workflow-edit/route.js
+++ app/pods/workflow-edit/route.js
@@ -1,4 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+     return [
+{  "id": 0,
+      "partner_id": 1,
+      "name": "only manual tasks 0",
+      "workflow_id": 303401,
+      "task_template_id": 5900,
+      "functional_group_id": 3000,
+      "seq_number": 1000,
+      "comments": "",
+      "is_skippable": false,
+      "created_at": 1410431527000,
+      "created_by": "User-99ca1727ee",
+      "updated_at": 1410431527000,
+      "updated_by": "User-2a8442550f",
+      "duration": null},
+{  "id": 1,
+      "partner_id": 1,
+      "name": "only manual tasks 1",
+      "workflow_id": 303401,
+      "task_template_id": 5900,
+      "functional_group_id": 3000,
+      "seq_number": 1000,
+      "comments": "",
+      "is_skippable": false,
+      "created_at": 1410431527000,
+      "created_by": "User-99ca1727ee",
+      "updated_at": 1410431527000,
+      "updated_by": "User-2a8442550f",
+      "duration": null},
+{  "id": 2,
+      "partner_id": 1,
+      "name": "only manual tasks 2",
+      "workflow_id": 303401,
+      "task_template_id": 5900,
+      "functional_group_id": 3000,
+      "seq_number": 1000,
+      "comments": "",
+      "is_skippable": false,
+      "created_at": 1410431527000,
+      "created_by": "User-99ca1727ee",
+      "updated_at": 1410431527000,
+      "updated_by": "User-2a8442550f",
+      "duration": null
+  }
+]
+  }
+
 });

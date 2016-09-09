98f26bcc429e5cd0b4ec96e8792e32aed3b012ac
erk-srs <erk-srs@outlook.com>
2016-09-09 10:38:19 +0530

    Few UI changes in workflow display

--- app/pods/workflow/model.js
+++ app/pods/workflow/model.js
@@ -1,13 +1,19 @@
 import DS from 'ember-data';
+import moment from 'moment';
+
 
 export default DS.Model.extend({
   partner_id: DS.attr('number'),
   name: DS.attr('string'),
   source_id: DS.attr('number'),
   is_default: DS.attr('boolean'),
-  created_at: DS.attr('number'),
+  created_at: DS.attr('date', {
+    defaultValue() {  return moment(new Date()) ; }
+  }),
   created_by: DS.attr('string'),
-  updated_at: DS.attr('number'),
+  updated_at: DS.attr('date', {
+    defaultValue() { return moment(new Date()) ; }
+  }),
   updated_by: DS.attr('string'),
   gate_ids: DS.hasMany('gates', { async: true }),
   is_active: DS.attr('boolean'),
